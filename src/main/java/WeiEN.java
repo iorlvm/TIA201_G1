92b440a5c50385c19669031c993bedada1c37dff
WeiEN <iorlvm001@gmail.com>
2024-06-02 15:47:41 +0800

    print 1 to 10

--- src/main/java/WeiEN.java
+++ src/main/java/WeiEN.java
@@ -1,5 +1,8 @@
 public class WeiEN {
     public static void main(String[] args) {
         System.out.println("嗨~~~");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
